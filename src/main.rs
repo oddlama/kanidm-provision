760fbc01b320bd668c19ee09547209a5d774f8b3
oddlama <oddlama@oddlama.org>
2024-03-05 21:32:23 +0100

    feat: add authentication

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use color_eyre::eyre::Result;
-use serde_json::json;
+use color_eyre::{
+    eyre::{eyre, Context, OptionExt, Result},
+    Section,
+};
+use reqwest::{
+    blocking::{Client, Response},
+    header::{HeaderMap, HeaderValue},
+};
+use serde_json::{json, Value};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,18 +21,114 @@ struct Cli {
     state: PathBuf,
 }
 
+trait ResponseExt {
+    fn get_json_response(self) -> Result<Value>;
+}
+impl ResponseExt for Response {
+    fn get_json_response(self) -> Result<Value> {
+        let status = self.status();
+        let json: Result<Value> = self
+            .text()
+            .wrap_err("Response had no body")
+            .and_then(|x| serde_json::from_str(&x).wrap_err("Response wasn't json"));
+
+        if !status.is_success() {
+            return Err(
+                eyre!("Server returned unsuccessful HTTP status ({status})").note(match json {
+                    Ok(ref value) => value.to_string(),
+                    Err(ref e) => e.to_string(),
+                }),
+            );
+        }
+
+        json
+    }
+}
+
+struct KanidmClient {
+    url: String,
+    client: Client,
+    idm_admin_headers: HeaderMap,
+    admin_headers: HeaderMap,
+}
+
+impl KanidmClient {
+    fn new(url: &str) -> Result<KanidmClient> {
+        let mut client = KanidmClient {
+            url: url.to_string(),
+            client: Client::new(),
+            idm_admin_headers: HeaderMap::new(),
+            admin_headers: HeaderMap::new(),
+        };
+
+        let (session_id, token) = client.auth("idm_admin", &std::env::var("KANIDM_PROVISION_IDM_ADMIN_TOKEN")?)?;
+        client
+            .idm_admin_headers
+            .insert("X-KANIDM-AUTH-SESSION-ID", HeaderValue::from_str(&session_id)?);
+        client
+            .idm_admin_headers
+            .insert("Authorization", HeaderValue::from_str(&format!("Bearer {token}"))?);
+
+        //let (session_id, token) = client.auth( "admin", &std::env::var("KANIDM_PROVISION_ADMIN_TOKEN")?);
+
+        Ok(client)
+    }
+
+    fn auth(&self, user: &str, password: &str) -> Result<(String, String)> {
+        let init_response = self
+            .client
+            .post(format!("{}/v1/auth", self.url))
+            .json(&json!({ "step": { "init": user } }))
+            .send()?
+            .error_for_status()?;
+
+        let session_id = init_response
+            .headers()
+            .get("X-KANIDM-AUTH-SESSION-ID")
+            .ok_or_eyre("No session id was returned by the server!")?;
+
+        let _begin_response = self
+            .client
+            .post(format!("{}/v1/auth", self.url))
+            .json(&json!({ "step": { "begin": "password" } }))
+            .header("X-KANIDM-AUTH-SESSION-ID", session_id)
+            .send()?
+            .get_json_response()?;
+
+        let cred_response = self
+            .client
+            .post(format!("{}/v1/auth", self.url))
+            .json(&json!({ "step": { "cred": { "password": password } } }))
+            .header("X-KANIDM-AUTH-SESSION-ID", session_id)
+            .send()?
+            .get_json_response()?;
+
+        let token = cred_response
+            .pointer("/state/success")
+            .and_then(|x| x.as_str())
+            .map(|x| x.to_string())
+            .ok_or_else(|| eyre!("No token found in response (incorrect password?): {cred_response:?}"))?;
+
+        Ok((session_id.to_str()?.to_string(), token))
+    }
+
+    fn a(&self) -> Result<()> {
+        let groups_response = self
+            .client
+            .get(format!("{}/v1/group", self.url))
+            .headers(self.idm_admin_headers.clone())
+            .send()?
+            .text()?;
+        println!("{}", groups_response);
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args = Cli::parse();
-
-    let client = reqwest::blocking::Client::new();
-    let url = args.url;
-    let _ = client
-        .post(format!("{url}/v1/auth"))
-        .json(&json!({
-            "a": "b"
-        }))
-        .send();
+    let kanidm_client = KanidmClient::new(&args.url)?;
+    kanidm_client.a()?;
 
     Ok(())
 }
