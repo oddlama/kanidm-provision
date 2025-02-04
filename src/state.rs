ab3fa7d59b76658ba98ccf50c2910329896dab6f
Tom Hubrecht <tom@hubrecht.ovh>
2025-02-04 14:32:43 +0100

    feat: Allow declaring memberless groups
    When a group is "memberless", then the list of members is left intact,
    which allows managing it imperatively.

--- src/state.rs
+++ src/state.rs
@@ -10,6 +10,8 @@ pub struct Group {
     #[serde(default = "default_true")]
     pub present: bool,
     pub members: Vec<String>,
+    #[serde(default = "default_false")]
+    pub memberless: bool,
 }
 
 #[derive(Debug, Deserialize)]
